60910d44e705f587cd062dbf42ef8f989685792f
taylorchiu <taylorparkerchiu@gmail.com>
2014-05-04 14:17:39 -0700

    First commit on the Druthers app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Druthers::Application.config.secret_key_base = '71e79c1e585676f7e744b9a4ef42001c243b1306924e3fec870cc9a35e1ad627d69c86f6c1ad15eba78e97fa87029878bb0a675391ca1d6605cb82b3983bcbb0'
