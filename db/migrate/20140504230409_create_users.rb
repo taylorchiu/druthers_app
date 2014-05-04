66f1957001de2502c5a283efd5366a1a2f29ee53
taylorchiu <taylorparkerchiu@gmail.com>
2014-05-04 16:20:52 -0700

    Adding the User model, migration and seed data

---
+++ db/migrate/20140504230409_create_users.rb
@@ -0,0 +1,13 @@
+# Forgot to add --no-test-framework when I created this model (oops)
+
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :remember_token
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: 'test2@gmail.com', password_digest: 'abcdef', remember_token: 'abcdef')
+
+User.create(email: 'test3@gmail.com', password_digest: '98765', remember_token: '98765')
+
+User.create(email: 'test4@gmail.com', password_digest: 'cow', remember_token: 'moo')
+
+User.create(email: 'test5@gmail.com', password_digest: 'cat', remember_token: 'meow')
