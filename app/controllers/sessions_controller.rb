84eed95f8f14b8c9fc1d96b4aed160a985a97780
taylorchiu <taylorparkerchiu@gmail.com>
2014-05-09 02:33:33 -0700

    precompiling assets

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Sign the user in and redirect to the user's show page.
       sign_in user
-      redirect_to books_path
+      redirect_to books_search_path
     else
       # Create an error message and re-render the signin form.
       flash[:error]='Invalid email address/password combination'
